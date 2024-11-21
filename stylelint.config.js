01731f9b3615de6f1430b317857a5fdd1a2a5e38
eleven <eleven.duan@outlook.com>
2024-11-21 14:52:49 +0800

    css module global

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,5 +1,13 @@
 const config = {
-  extends: ["stylelint-config-standard"]
+  extends: ["stylelint-config-standard"],
+  rules: {
+    "selector-pseudo-class-no-unknown": [
+      true,
+      {
+        ignorePseudoClasses: ["global"]
+      }
+    ]
+  }
 };
 
 export default config;
