03cbd0e9ab5e39681aeebdd6e49ab36cf4927ebe
eleven <eleven.duan@outlook.com>
2025-03-13 11:56:34 +0800

    update PageLoading

--- src/components/PageLoading/index.tsx
+++ src/components/PageLoading/index.tsx
@@ -1,15 +1,25 @@
+import { useEffect, useState } from "react";
 import styles from "./index.module.css";
 
 type PropsType = {
   show?: boolean;
   text?: string;
+  duration?: number; // ms
 };
 
 export const PageLoading = (props: PropsType) => {
-  const { show, text = "加载中..." } = props;
+  const { show, text = "加载中...", duration = 400 } = props;
+  const [fade, setFade] = useState<boolean>();
+
+  useEffect(() => {
+    setTimeout(() => setFade(show), !show && fade ? duration : 0);
+  }, [show]);
 
   return (
-    <div className={`${styles.mask} ${show ? styles.show : ""}`}>
+    <div
+      className={`${styles.mask} ${show || fade ? styles.block : ""} ${show && fade ? styles.show : ""}`}
+      style={{ transitionDuration: duration + "ms" }}
+    >
       <div className={styles.outer}>
         <div className={styles.inner}>
           <div>
