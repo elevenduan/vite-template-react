99f856adb4dceed3ca322e1496f1cf31d26fd533
eleven <eleven.duan@outlook.com>
2025-09-09 16:27:56 +0800

    update crypto

--- src/utils/crypto.ts
+++ src/utils/crypto.ts
@@ -1,18 +1,33 @@
 import CryptoJS from "crypto-js";
+import JSEncrypt from "jsencrypt";
 
-const key = CryptoJS.enc.Utf8.parse("b1941412757b6088e6ceef4b04276ee6");
-const iv = CryptoJS.enc.Utf8.parse("0000000000000000");
-// 'CBC' | 'CFB' | 'CTR' | 'OFB' | 'ECB'; // CBC (the default)
-// 'Pkcs7' | 'Iso97971' | 'AnsiX923' | 'Iso10126' | 'ZeroPadding' | 'NoPadding'; // Pkcs7 (the default)
-const opt = { mode: CryptoJS.mode.CBC, padding: CryptoJS.pad.ZeroPadding, iv };
-
-export function aes(str: string) {
-  const encrypted = CryptoJS.AES.encrypt(str, key, opt);
-  const hexStr = encrypted.ciphertext.toString().toUpperCase();
-  return hexStr;
+// AES
+const aesKey = CryptoJS.enc.Utf8.parse("aesKey");
+const aesIv = CryptoJS.enc.Utf8.parse("0000000000000000");
+const aesCfg = { iv: aesIv, mode: CryptoJS.mode.CBC, padding: CryptoJS.pad.Pkcs7 };
+export function aesEncrypt(str: string) {
+  const encrypted = CryptoJS.AES.encrypt(str, aesKey, aesCfg);
+  // const hexStr = encrypted.ciphertext.toString().toUpperCase();
+  const base64Str = encrypted.toString().toUpperCase();
+  return base64Str;
+}
+export function aesDecrypt(str: string) {
+  const bytes = CryptoJS.AES.decrypt(str, aesKey, aesCfg);
+  const text = bytes.toString(CryptoJS.enc.Utf8);
+  return text;
 }
 
+// MD5
 export function md5(str: string) {
   const base64Str = CryptoJS.MD5(str).toString().toUpperCase();
   return base64Str;
+}
+
+// RSA
+const rsaPublicKey = "rsaPublicKey";
+const rsaCrypt = new JSEncrypt();
+rsaCrypt.setPublicKey(rsaPublicKey);
+export function rsaEncrypt(str: string) {
+  const base64Str = rsaCrypt.encrypt(str);
+  return base64Str || "";
 }
