e7d775cecf51bd6b145466bc197294114966c174
eleven <eleven.duan@outlook.com>
2026-09-07 14:39:31 +0800

    style

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,6 +2,8 @@ const config = {
   extends: ["stylelint-config-standard"],
   rules: {
     "selector-pseudo-class-no-unknown": [true, { ignorePseudoClasses: ["global"] }],
+    "selector-class-pattern": ["^(?:[a-z][a-zA-Z0-9]*|[a-z][a-z0-9]*(?:-[a-z0-9]+)+)$", { message: "Expected class selector to be kebab-case or camelCase" }],
+    "no-descending-specificity": null,
   },
 };
 
